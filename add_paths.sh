0dcd3268c05326d47a3de847650b9ca13737ad69
ISLa - Intelligent Systems Lab <isla.univr@gmail.com>
2023-03-31 09:27:11 +0200

    Add files via upload

---
+++ add_paths.sh
@@ -0,0 +1,3 @@
+echo 'export PATH=$PATH:/usr/local/cuda/bin' >> ~/.bashrc 
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib' >> ~/.bashrc 
